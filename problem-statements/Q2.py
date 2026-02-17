4f0bc2d61833a66122680b1f229dcb5e6d2b26a6
Havish-Wurity <havishwurity@gmail.com>
2026-02-17 15:20:35 +0530

    Update Q2.py

--- problem-statements/Q2.py
+++ problem-statements/Q2.py
@@ -1,14 +1,16 @@
 def convert_seconds(total_seconds):
-    minutes = total_seconds // 60
-    seconds = total_seconds % 60
-    
-    return minutes,seconds
-
-total_seconds=int(input('Enter the number of seconds:'))
-if total_seconds>=86400:
+    if total_seconds>=86400:
     print('The total number of seconds exceeded to that of a day')
 elif total_seconds<0:
     print('the total number of seconds are invalid.Please enter valid number of seconds')
-else:
-    minutes,seconds=convert_seconds(total_seconds)
-    print('{}m {}s'.format(minutes,seconds))
+ else:
+    minutes = total_seconds // 60
+    seconds = total_seconds % 60
+    
+    return '{}m {}s'.format(minutes,seconds)   
+    
+
+
+
+
+   
