8bb37754263c9730eb9707e605224200e27041e9
Havish-Wurity <havishwurity@gmail.com>
2026-02-17 16:50:27 +0530

    Create Q7.py

---
+++ problem-statements/Q7.py
@@ -0,0 +1,10 @@
+def count_inventory(fruit_list):
+    inventory = {}
+    
+    for fruit in fruit_list:
+        if fruit in inventory:
+            inventory[fruit] += 1
+        else:
+            inventory[fruit] = 1
+    
+    return inventory
